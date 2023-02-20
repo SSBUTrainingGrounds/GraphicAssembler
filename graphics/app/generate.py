438ddbac9ba8bf9c6f25c0f1a22c749e84dbe881
Parz <55297098+PatrickMiller728@users.noreply.github.com>
2023-02-19 22:18:49 -0500

    added characters and test text

--- graphics/app/generate.py
+++ graphics/app/generate.py
@@ -1,14 +1,26 @@
 import os
-from graphics.definitions import THUMBNAIL_DIR, OUTPUT_DIR
+from graphics.definitions import THUMBNAIL_DIR, RENDERS_DIR, OUTPUT_DIR
 
-# Import ImageDraw for Text
 # Import ImageFont for Fonts
-from PIL import Image
+from PIL import Image, ImageDraw
+
+def crop_character(image, crop):
+    img_width, img_height = image.size
+    return image.crop(((img_width - crop[0]) // 2,
+                       (img_height - crop[1]) // 2,
+                       (img_width + crop[0]) // 2,
+                       (img_height + crop[1]) // 2))
+
 
 def generate_thumbnail(tournament):
-
     SIZE = (1920, 1080)
+    CHARACTER_BOX = (960, 800)
+
     canvas = Image.new('RGBA', SIZE, (0, 0, 0))
+    draw = ImageDraw.Draw(canvas)
+
+    # Output Path
+    output = os.path.join(OUTPUT_DIR, "test1.png")
 
     # Paths to Template Items
     background_path = os.path.join(THUMBNAIL_DIR, "Thumbnail-Background.png")
@@ -16,6 +28,8 @@ def generate_thumbnail(tournament):
     tournament_path = os.path.join(THUMBNAIL_DIR, tournament)
     character_background_path = os.path.join(tournament_path, "Thumbnail-CharacterBackground.png")
     text_background_path = os.path.join(tournament_path, "Thumbnail-TextBackground.png")
+    character_paths = [os.path.join(RENDERS_DIR, "02-Donkey Kong\\01.png"),
+                       os.path.join(RENDERS_DIR, "01-Mario\\01.png")]
 
     # Background
     background = Image.open(background_path, mode="r")
@@ -25,6 +39,21 @@ def generate_thumbnail(tournament):
     character_background = Image.open(character_background_path, mode="r")
     canvas.paste(character_background, (0, 0), character_background)
 
+    # TODO: This needs to be a loop or a function
+    # Player 1 Character
+    character_one = Image.open(character_paths[0], mode="r")
+    width_one, height_one = character_one.size
+    character_one = character_one.resize((int(width_one * .75), int(height_one * .75)))
+    character_one = crop_character(character_one, CHARACTER_BOX)
+    canvas.paste(character_one, (0, 140), character_one)
+
+    # Player 2 Character
+    character_two = Image.open(character_paths[1], mode="r")
+    width_two, height_two = character_two.size
+    character_two = character_two.resize((int(width_two * .75), int(height_two * .75)))
+    character_two = crop_character(character_two, CHARACTER_BOX)
+    canvas.paste(character_two, (960, 140), character_two)
+
     # Text Background
     text_background = Image.open(text_background_path, mode="r")
     canvas.paste(text_background, (0, 0), text_background)
@@ -33,7 +62,7 @@ def generate_thumbnail(tournament):
     vs = Image.open(vs_path, mode="r")
     canvas.paste(vs, (0, 0), vs)
 
-    # Output Path
-    output = os.path.join(OUTPUT_DIR, "test1.png")
+    # Text
+    draw.text((100, 100), "Test")
 
     canvas.save(output, format="PNG")
--- graphics/definitions.py
+++ graphics/definitions.py
@@ -3,6 +3,10 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 RESOURCES_DIR = os.path.join(ROOT_DIR, 'resources')
 THUMBNAIL_DIR = os.path.join(RESOURCES_DIR, 'thumbnail')
+# Hard coded for now
+# These are the renders from the other repo
+# Consider making them a submodule?
+RENDERS_DIR = "D:\\TrainingGrounds-Graphics\\Character Renders"
 
 APP_DIR = os.path.join(ROOT_DIR, 'app')
 UI_DIR = os.path.join(ROOT_DIR, 'ui')
