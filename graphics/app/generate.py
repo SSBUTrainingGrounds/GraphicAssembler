930485f6d1c89985d433300f8eeb067796c1c468
atomflunder <80397293+atomflunder@users.noreply.github.com>
2023-02-26 00:31:39 +0100

    Add Offset and Zoom Sliders

--- graphics/app/generate.py
+++ graphics/app/generate.py
@@ -3,8 +3,7 @@ import os
 # Import ImageFont for Fonts
 from PIL import Image, ImageDraw, ImageFont
 
-from graphics.definitions import (FONTS_DIR, OUTPUT_DIR, RENDERS_DIR,
-                                  THUMBNAIL_DIR)
+from graphics.definitions import FONTS_DIR, OUTPUT_DIR, RENDERS_DIR, THUMBNAIL_DIR
 
 SIZE = (1920, 1080)
 CHARACTER_BOX = (960, 800)
@@ -21,27 +20,34 @@ def get_character_path(character: str, alt: str = "01") -> str:
     return os.path.join(RENDERS_DIR, f"{character}/{alt}.png")
 
 
-def crop_character(image: Image, crop: tuple[int, int]) -> Image:
+def crop_character(
+    image: Image, crop: tuple[int, int], offset: tuple[int, int]
+) -> Image:
     img_width, img_height = image.size
-    return image.crop(((img_width - crop[0]) // 2,
-                       (img_height - crop[1]) // 2,
-                       (img_width + crop[0]) // 2,
-                       (img_height + crop[1]) // 2))
+
+    return image.crop(
+        (
+            (img_width - crop[0] + offset[0]) // 2,
+            (img_height - crop[1] + offset[1]) // 2,
+            (img_width + crop[0] + offset[0]) // 2,
+            (img_height + crop[1] + offset[1]) // 2,
+        )
+    )
 
 
-def resize_character(image: Image) -> Image:
+def resize_character(image: Image, zoom: int) -> Image:
     width_one, height_one = image.size
     if width_one > 960:
         factor = 960 / width_one
-        width_one = int(width_one * factor)
-        height_one = int(height_one * factor)
+        width_one = int(width_one * factor * zoom / 100)
+        height_one = int(height_one * factor * zoom / 100)
     return image.resize((width_one, height_one))
 
 
-def generate_character_image(path: str) -> Image:
+def generate_character_image(path: str, offset: tuple[int, int], zoom: int) -> Image:
     character = Image.open(path, mode="r")
-    character = resize_character(character)
-    character = crop_character(character, CHARACTER_BOX)
+    character = resize_character(character, zoom)
+    character = crop_character(character, CHARACTER_BOX, offset)
     return character
 
 
@@ -49,31 +55,39 @@ def draw_thumbnail_text(text: str, into: Image, center: tuple[int, int]):
     multiple = 3
 
     # TODO: Make function that determines max size for font based on length of input str
-    syncopate = ImageFont.truetype(os.path.join(FONTS_DIR, 'Syncopate-Bold.ttf'), 120 * multiple)
+    syncopate = ImageFont.truetype(
+        os.path.join(FONTS_DIR, "Syncopate-Bold.ttf"), 120 * multiple
+    )
     wi, hi = syncopate.getsize(text)
 
-    img = Image.new('RGBA', (wi, hi), (0, 0, 0, 0))
+    img = Image.new("RGBA", (wi, hi), (0, 0, 0, 0))
     draw = ImageDraw.Draw(img)
     draw.text((wi / 2, hi / 2), text, (255, 255, 255), syncopate, anchor="mm")
 
     img = img.rotate(1.58, expand=True)
     img = img.resize((wi // multiple, hi // multiple), resample=Image.ANTIALIAS)
 
-    into.alpha_composite(img, (center[0] - (wi // multiple // 2), center[1] + (hi // multiple // 2)))
+    into.alpha_composite(
+        img, (center[0] - (wi // multiple // 2), center[1] + (hi // multiple // 2))
+    )
 
 
 def generate_thumbnail(data) -> Image:
-    canvas = Image.new('RGBA', SIZE)
+    canvas = Image.new("RGBA", SIZE)
 
     # TODO: This is allocating every time its run, move to constants
     # Paths to Template Items
     background_path = os.path.join(THUMBNAIL_DIR, "Thumbnail-Background.png")
     vs_path = os.path.join(THUMBNAIL_DIR, "Thumbnail-VS-01.png")
     tournament_path = os.path.join(THUMBNAIL_DIR, data["tournament"])
-    character_background_path = os.path.join(tournament_path, "Thumbnail-CharacterBackground.png")
+    character_background_path = os.path.join(
+        tournament_path, "Thumbnail-CharacterBackground.png"
+    )
     text_background_path = os.path.join(tournament_path, "Thumbnail-TextBackground.png")
-    character_paths = [get_character_path(data["players"][0]["character"], data["players"][0]["alt"]),
-                       get_character_path(data["players"][1]["character"], data["players"][1]["alt"])]
+    character_paths = [
+        get_character_path(data["players"][0]["character"], data["players"][0]["alt"]),
+        get_character_path(data["players"][1]["character"], data["players"][1]["alt"]),
+    ]
 
     # Background
     background = Image.open(background_path, mode="r")
@@ -84,10 +98,20 @@ def generate_thumbnail(data) -> Image:
     canvas.alpha_composite(character_background)
 
     # Player 1 Character
-    canvas.alpha_composite(generate_character_image(character_paths[0]), POSITION[0])
+    canvas.alpha_composite(
+        generate_character_image(
+            character_paths[0], data["players"][0]["offset"], data["players"][0]["zoom"]
+        ),
+        POSITION[0],
+    )
 
     # Player 2 Character
-    canvas.alpha_composite(generate_character_image(character_paths[1]), POSITION[1])
+    canvas.alpha_composite(
+        generate_character_image(
+            character_paths[1], data["players"][1]["offset"], data["players"][1]["zoom"]
+        ),
+        POSITION[1],
+    )
 
     # Text Background
     text_background = Image.open(text_background_path, mode="r")
--- graphics/ui/views/MainWindow.py
+++ graphics/ui/views/MainWindow.py
@@ -4,6 +4,7 @@ from graphics.ui.widgets.AltDropdown import AltDropdown
 from graphics.ui.widgets.CharacterDropdown import CharacterDropdown
 from graphics.ui.widgets.CreateButton import CreateButton
 from graphics.ui.widgets.ImagePreview import ImagePreview
+from graphics.ui.widgets.OffsetSlider import OffsetSlider
 from graphics.ui.widgets.PlayerTag import PlayerTag
 
 
@@ -12,7 +13,7 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("TG Graphics")
-        self.resize(400, 300)
+        self.resize(600, 450)
         self.grid = QGridLayout()
 
         layout = QHBoxLayout()
@@ -22,13 +23,17 @@ class MainWindow(QMainWindow):
             "players": [
                 {
                     "tag": "NIFARES",
-                    "character": "",
-                    "alt": "",
+                    "character": "01-Mario",
+                    "alt": "01",
+                    "offset": [0, 0],
+                    "zoom": 100,
                 },
                 {
                     "tag": "PARZ",
-                    "character": "",
-                    "alt": "",
+                    "character": "02-Donkey Kong",
+                    "alt": "01",
+                    "offset": [0, 0],
+                    "zoom": 100,
                 },
             ],
             "tournament": "tos",
@@ -47,6 +52,9 @@ class MainWindow(QMainWindow):
         player_tag_one = PlayerTag(player_one)
         player_tag_two = PlayerTag(player_two)
 
+        offset_sliders_one = OffsetSlider(player_one)
+        offset_sliders_two = OffsetSlider(player_two)
+
         preview = ImagePreview(data)
 
         button = CreateButton(data)
@@ -54,12 +62,14 @@ class MainWindow(QMainWindow):
         self.grid.addWidget(player_tag_one, 0, 0)
         self.grid.addWidget(character_dropdown_one, 0, 1)
         self.grid.addWidget(alt_dropdown_one, 0, 2)
+        self.grid.addWidget(offset_sliders_one, 1, 0, 2, 3)
 
         self.grid.addWidget(player_tag_two, 0, 3)
         self.grid.addWidget(character_dropdown_two, 0, 4)
         self.grid.addWidget(alt_dropdown_two, 0, 5)
+        self.grid.addWidget(offset_sliders_two, 1, 3, 2, 3)
 
-        self.grid.addWidget(preview, 1, 0, 1, 6)
+        self.grid.addWidget(preview, 3, 0, 1, 6)
 
         character_dropdown_one.currentTextChanged.connect(lambda: preview.update())
         character_dropdown_two.currentTextChanged.connect(lambda: preview.update())
@@ -69,6 +79,12 @@ class MainWindow(QMainWindow):
 
         player_tag_one.textChanged.connect(lambda: preview.update())
         player_tag_two.textChanged.connect(lambda: preview.update())
+
+        for child in offset_sliders_one.children:
+            child.valueChanged.connect(lambda: preview.update())
+
+        for child in offset_sliders_two.children:
+            child.valueChanged.connect(lambda: preview.update())
 
         self.grid.addWidget(button, 0, 6)
 
